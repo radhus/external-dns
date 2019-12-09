976e4354e048ed6c5e0da4bf5d58594f145636b1
William Johansson <radar@radhuset.org>
2019-12-09 01:07:21 +0100

    WIP disable istio

--- source/store.go
+++ source/store.go
@@ -29,8 +29,6 @@ import (
 	contour "github.com/heptio/contour/apis/generated/clientset/versioned"
 	"github.com/linki/instrumented_http"
 	log "github.com/sirupsen/logrus"
-	istiocrd "istio.io/istio/pilot/pkg/config/kube/crd"
-	istiomodel "istio.io/istio/pilot/pkg/model"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -64,7 +62,6 @@ type Config struct {
 // ClientGenerator provides clients
 type ClientGenerator interface {
 	KubeClient() (kubernetes.Interface, error)
-	IstioClient() (istiomodel.ConfigStore, error)
 	CloudFoundryClient(cfAPPEndpoint string, cfUsername string, cfPassword string) (*cfclient.Client, error)
 	ContourClient() (contour.Interface, error)
 }
@@ -76,7 +73,6 @@ type SingletonClientGenerator struct {
 	KubeMaster     string
 	RequestTimeout time.Duration
 	kubeClient     kubernetes.Interface
-	istioClient    istiomodel.ConfigStore
 	cfClient       *cfclient.Client
 	contourClient  contour.Interface
 	kubeOnce       sync.Once
@@ -92,15 +88,6 @@ func (p *SingletonClientGenerator) KubeClient() (kubernetes.Interface, error) {
 		p.kubeClient, err = NewKubeClient(p.KubeConfig, p.KubeMaster, p.RequestTimeout)
 	})
 	return p.kubeClient, err
-}
-
-// IstioClient generates an istio client if it was not created before
-func (p *SingletonClientGenerator) IstioClient() (istiomodel.ConfigStore, error) {
-	var err error
-	p.istioOnce.Do(func() {
-		p.istioClient, err = NewIstioClient(p.KubeConfig)
-	})
-	return p.istioClient, err
 }
 
 // CloudFoundryClient generates a cf client if it was not created before
@@ -165,16 +152,6 @@ func BuildWithConfig(source string, p ClientGenerator, cfg *Config) (Source, err
 			return nil, err
 		}
 		return NewIngressSource(client, cfg.Namespace, cfg.AnnotationFilter, cfg.FQDNTemplate, cfg.CombineFQDNAndAnnotation, cfg.IgnoreHostnameAnnotation)
-	case "istio-gateway":
-		kubernetesClient, err := p.KubeClient()
-		if err != nil {
-			return nil, err
-		}
-		istioClient, err := p.IstioClient()
-		if err != nil {
-			return nil, err
-		}
-		return NewIstioGatewaySource(kubernetesClient, istioClient, cfg.IstioIngressGatewayServices, cfg.Namespace, cfg.AnnotationFilter, cfg.FQDNTemplate, cfg.CombineFQDNAndAnnotation, cfg.IgnoreHostnameAnnotation)
 	case "cloudfoundry":
 		cfClient, err := p.CloudFoundryClient(cfg.CFAPIEndpoint, cfg.CFUsername, cfg.CFPassword)
 		if err != nil {
@@ -241,36 +218,6 @@ func NewKubeClient(kubeConfig, kubeMaster string, requestTimeout time.Duration) 
 	}
 
 	log.Infof("Created Kubernetes client %s", config.Host)
-
-	return client, nil
-}
-
-// NewIstioClient returns a new Istio client object. It uses the configured
-// KubeConfig attribute to connect to the cluster. If KubeConfig isn't provided
-// it defaults to using the recommended default.
-// NB: Istio controls the creation of the underlying Kubernetes client, so we
-// have no ability to tack on transport wrappers (e.g., Prometheus request
-// wrappers) to the client's config at this level. Furthermore, the Istio client
-// constructor does not expose the ability to override the Kubernetes master,
-// so the Master config attribute has no effect.
-func NewIstioClient(kubeConfig string) (*istiocrd.Client, error) {
-	if kubeConfig == "" {
-		if _, err := os.Stat(clientcmd.RecommendedHomeFile); err == nil {
-			kubeConfig = clientcmd.RecommendedHomeFile
-		}
-	}
-
-	client, err := istiocrd.NewClient(
-		kubeConfig,
-		"",
-		istiomodel.ConfigDescriptor{istiomodel.Gateway},
-		"",
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Info("Created Istio client")
 
 	return client, nil
 }
